b6b294c809dc64c8aa13e780b71907c265991212
ishpeeedy <anudgesharma@gmail.com>
2025-10-04 21:48:14 +0530

    add background

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,8 @@ import UrlForm from '../components/UrlForm';
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold text-center mb-6">URL Shortener</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center ">
+      <div className ="p-8 rounded-lg w-full max-w-md">
         <UrlForm />
       </div>
     </div>
