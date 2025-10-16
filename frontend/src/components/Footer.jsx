37732aa78e8ee6b2005a3fcf917be1afdedd4e67
ishpeeedy <anudgesharma@gmail.com>
2025-10-16 20:45:51 +0530

    add logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,11 +11,14 @@ const Footer = () => {
     <footer className="w-full bg-main border-t mt-4">
       <div className="max-w-4xl mx-auto py-4">
         <div className="flex justify-between items-center">
-          <p className="text-center text-sm text-main-foreground">
-            {' '}
-            <a href="https://github.com/ishpeeedy/BlinkURL" class="underline">
+          <div className="flex items-center gap-4 text-sm text-main-foreground">
+            <a
+              href="https://github.com/ishpeeedy/BlinkURL"
+              className="underline flex items-center gap-2"
+            >
+              <img src="/logo64.png" width={'24'} alt="" />
               <span>BlinkURL Repo</span>
-              <svg viewBox="0 0 13 20">
+              <svg viewBox="0 0 13 20" className="w-4 h-4">
                 <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
               </svg>
             </a>
@@ -26,15 +29,32 @@ const Footer = () => {
               <HoverCardContent>
                 The SVG illustrations used in this project are sourced from{' '}
                 <a href="https://storyset.com/">Storyset </a>, a platform that
-                offers high-quality, customizable illustrations for various
-                themes and purposes.
+                offers high-quality, customizable illustrations.
+                <div>
+                  {' '}
+                  Logo icon made by{' '}
+                  <a
+                    href="https://www.flaticon.com/authors/freepik"
+                    title="Freepik"
+                  >
+                    {' '}
+                    Freepik{' '}
+                  </a>{' '}
+                  from{' '}
+                  <a href="https://www.flaticon.com/" title="Flaticon">
+                    www.flaticon.com
+                  </a>
+                </div>
               </HoverCardContent>
             </HoverCard>
-          </p>
+          </div>
           <p className="text-center text-sm text-main-foreground">
-            <a href="https://github.com/ishpeeedy" class="underline">
+            <a
+              href="https://github.com/ishpeeedy"
+              className="underline flex items-center gap-2"
+            >
               <span>Made with 🖤 by ishpeeedy</span>
-              <svg viewBox="0 0 13 20">
+              <svg viewBox="0 0 13 20" className="w-4 h-4">
                 <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
               </svg>
             </a>
--- frontend/src/components/ui/hover-card.jsx
+++ frontend/src/components/ui/hover-card.jsx
@@ -1,24 +1,22 @@
-import * as HoverCardPrimitive from "@radix-ui/react-hover-card"
+import * as HoverCardPrimitive from '@radix-ui/react-hover-card';
 
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
-function HoverCard({
-  ...props
-}) {
+function HoverCard({ ...props }) {
   return <HoverCardPrimitive.Root data-slot="hover-card" {...props} />;
 }
 
-function HoverCardTrigger({
-  ...props
-}) {
-  return (<HoverCardPrimitive.Trigger data-slot="hover-card-trigger" {...props} />);
+function HoverCardTrigger({ ...props }) {
+  return (
+    <HoverCardPrimitive.Trigger data-slot="hover-card-trigger" {...props} />
+  );
 }
 
 function HoverCardContent({
   className,
-  align = "center",
+  align = 'center',
   sideOffset = 4,
   ...props
 }) {
@@ -28,11 +26,12 @@ function HoverCardContent({
       align={align}
       sideOffset={sideOffset}
       className={cn(
-        "z-50 w-64 rounded-base border-2 border-border bg-main p-4 font-base text-main-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--radix-hover-card-content-transform-origin)",
+        'z-50 w-64 rounded-base border-2 border-border bg-main p-4 font-base text-main-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 origin-(--radix-hover-card-content-transform-origin)',
         className
       )}
-      {...props} />
+      {...props}
+    />
   );
 }
 
-export { HoverCard, HoverCardTrigger, HoverCardContent }
+export { HoverCard, HoverCardTrigger, HoverCardContent };
